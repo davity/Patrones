058f94253c10a88bc4465b23b03f2e9af446bc70
David Moran <davitymola@gmail.com>
2012-05-01 13:14:51 +0200

    Nuevo proyecto de NetBeans

---
+++ NetTower/src/nettower/NetTower.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nettower;
+
+/**
+ *
+ * @author David Moran Diaz
+ */
+public class NetTower {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
