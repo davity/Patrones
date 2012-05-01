8cd97ea3c06ce1dd2012dbc4288733c83ac04048
David Moran <davitymola@gmail.com>
2012-05-01 18:41:14 +0200

    Añadida una clase de sonidos y un sonido cada vez que rebota el cuadrado

---
+++ NetTower/src/nettower/Sound.java
@@ -0,0 +1,68 @@
+package nettower;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Sound {
+    public static class Clips {
+        public Clip[] clips;
+        private int p;
+        private int count;
+        
+        public Clips(byte[] buffer, int count) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+            if (buffer==null) return;
+            
+            clips = new Clip[count];
+            this.count = count;
+            for (int i=0; i<count; i++) {
+                clips[i] = AudioSystem.getClip();
+                clips[i].open(AudioSystem.getAudioInputStream(new ByteArrayInputStream(buffer)));
+            }
+        }
+        
+        public void play() {
+            if (clips==null) return;
+
+            clips[p].stop();
+            clips[p].setFramePosition(0);
+            clips[p].start();
+            p++;
+            if (p>=count) p = 0;
+        }
+    }
+    
+    // Sonidos a cargar cuando se inicia la aplicacion
+    public static Clips bounce = load("/bounce.wav", 4);
+
+
+    private static Clips load(String name, int count) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataInputStream dis = new DataInputStream(Sound.class.getResourceAsStream(name));
+            byte[] buffer = new byte[1024];
+            int read = 0;
+            while ((read = dis.read(buffer)) >= 0) {
+                baos.write(buffer, 0, read);
+            }
+            dis.close();
+            
+            byte[] data = baos.toByteArray();
+            return new Clips(data, count);
+        } catch (Exception e) {
+            try {
+                return new Clips(null, 0);
+            } catch (Exception ee) {
+                return null;
+            }
+        }
+    }
+
+    public static void touch() {
+    }
+}
